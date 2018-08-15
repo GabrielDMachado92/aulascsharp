a34abf36f6f3896fc2cfa32501ac08dbe06e2d7b
Gabriel Machado <gabriel.d.machado92@gmail.com>
2018-08-14 21:28:04 -0300

    Criando projeto aula 1408 entity framework 01

---
+++ Aulas/Aula1408_EntityFramework1/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1408_EntityFramework1
+{
+
+    /*
+           1. O que significa ORM?
+           Mapeamento Objeto-Relacional(ORM) é um framework ou conjunto  de classes que cria uma ponte entre 
+           o modelo relocional e o modelo orientado a objetos, facilitando o trato com as informações a serem armazenadas ou 
+           recuperadadas do banco de dados.
+
+           2. O que é o Entity Framework?
+           É um framework objeto-relacional que permite que o .NET consiga 
+           trabalhar com o banco de dados relacional usando seus objetos. 
+           Eliminando o tempo que os programadores perdiam montando as querys.
+
+           Algumas de suas principais funcionalidades são que ele é multiplataforma, ou seja, 
+           roda em qualquer sistema operacional, permite LINQ querys, permite inserções assíncronas 
+           e usa do cache para aumentar performance.
+
+           Abstrai o ADO.Net para a conversa com o banco de dados.
+
+           3. Para que serve a classe de contexto?
+           Representa uma conexão de banco de dados e um conjunto de tabelas.
+           Cotexto contém os métodos e propriedades para comunicação do banco de dados.
+
+           4. O que é e para que serve o DbSet?
+           É uma classe do pacote do Entity Framework que serve para mapear um objeto que será relacionado ao banco de dados.
+           Usado na configuração da classe de contexto. 
+
+            Instalando Entity Framework
+
+
+        */
+    public class Class1
+    {
+       
+        
+
+
+    }
+}
